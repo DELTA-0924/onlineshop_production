4a5d077e459b34f75fc90dae7c8653ade67d147b
Timur <scorpik1992@gmail.com>
2024-07-22 17:47:37 +0600

    remain a ui

--- frontend/shop/src/app/services/product.service.ts
+++ frontend/shop/src/app/services/product.service.ts
@@ -6,7 +6,7 @@ import { Product } from '../common/Product';
   providedIn: 'root'
 })
 export class ProductService {
-  private  url:string="/api"
+  private  url:string="http://localhost:8080/api"
   constructor(private http:HttpClient) { }
   get():Observable<any>{
     let limit=new HttpParams().append("limit",5);
